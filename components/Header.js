38b76af347ff0ba3477df71714007c5d67d2503e
Mugdin12 <mugdin.krnjic20@gmail.com>
2022-08-08 18:19:44 +0200

    create server side function and pass it to client side

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
           <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
         </div>
         <Image
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
+        src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
         width={200}
         height={80}
         className="cursor-pointer active:brightness-110"
